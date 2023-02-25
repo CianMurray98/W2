48b519af395968c60d5e758e51ec439dd3a4d0b3
cian murray <g00358011@atu.ie>
2023-02-25 14:33:04 +0000

    Created Project

---
+++ src/main/java/ie/atu/week2/calculator.java
@@ -0,0 +1,16 @@
+package ie.atu.week2;
+
+public class calculator {
+    int FirstNumber;
+    int SecondNumber;
+
+    public calculator(int FirstNumber, int SecondNumber) {
+        this.FirstNumber = FirstNumber;
+        this.SecondNumber = SecondNumber;
+    }
+
+    public int add(){
+        int total = FirstNumber + SecondNumber;
+        return total;
+    }
+}
